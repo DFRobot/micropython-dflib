2bb784ac463f9639ab3ec50d3cc41ec624153b1c
nanawangDFR <nana.wang@dfrobot.com>
2017-08-16 17:19:32 +0800

    add bme280 micropython lib

---
+++ BME280/examples/BME280Demo.py
@@ -0,0 +1,7 @@
+from machine import Pin
+import BME280
+i2c = I2C(scl=Pin(22),sda=Pin(21), freq=10000)
+bme = BME280.BME280(i2c=i2c)
+while True:
+  print(bme.temperature, bme.pressure, bme.humidity)
+  time.sleep(1)
