075977709217f6fc0f150a1d21ed51f6e8580a67
nanawangDFR <nana.wang@dfrobot.com>
2017-09-21 16:28:19 +0800

    add ADXL345 micropython lib

--- ADXL345/examples/ADXL345_Demo.py
+++ ADXL345/examples/ADXL345_Demo.py
@@ -6,11 +6,11 @@ i2c = I2C(scl=Pin(22),sda=Pin(21), freq=10000)
 adx = ADXL345.ADXL345(i2c)
 
 while True:
-    x,y,z = adx.get_xyzValue()
+    x=adx.xValue
+    y=adx.yValue
+    z=adx.zValue
     print('The acceleration info of x, y, z are:%d,%d,%d'%(x,y,z))
     roll,pitch = adx.RP_calculate(x,y,z)
     print('roll=',roll)
     print('pitch=',pitch)
     time.sleep_ms(50)
-
-
