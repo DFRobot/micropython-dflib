9e4f9cb21292d8220547cda695ff72275fa0cc78
mengbishu <30721964+mengbishu@users.noreply.github.com>
2017-10-13 14:53:35 +0800

    Update Servo.py

--- Servo/lib/Servo.py
+++ Servo/lib/Servo.py
@@ -15,14 +15,12 @@ class Servo:
 
   
   def angle(self,ang):
-#    self.turn = ang*(195/180) + 60
     if ang >= 180:
       ang=180
     if ang < 0:
       ang=0
     
     self.turn = self._map(ang,0,180,self.min,self.max)
-    print(ang)
     self.pwm.duty((int)(self.turn))
     self.lastStat=ang
 
